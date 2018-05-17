60e1f7dd9d28c4156131e84df367b353122c2af6
Anna Shainurova <amphiphile@yandex.ru>
2018-05-17 15:00:15 +0300

    поиск идентификатора якоря в начале строки

--- src/main/java/com/amphiphile/Main.java
+++ src/main/java/com/amphiphile/Main.java
@@ -11,10 +11,13 @@ import org.asciidoctor.ast.impl.ListImpl;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Main {
 
+    private static ArrayList<ExtendedBlock> allBlocks = new ArrayList<>();
     private static ArrayList<ExtendedBlock> unidentifiedBlocks = new ArrayList<>();
 
     public static void main(String[] args) {
@@ -56,59 +59,64 @@ public class Main {
         if (!(block.getContext().equals("document") ||
                 block.getContext().equals("preamble"))) {
 
-            if (block.getId() == null) {
-                addUnidentifiedBlock(block);
-            } else {
-                checkNestedItemsId(block);
-            }
+            addBlock(block);
+            checkNestedItems(block);
         }
 
     }
 
-    private static void addUnidentifiedBlock(StructuralNode unidentifiedBlock) {
+    private static void addBlock(StructuralNode block) {
         ExtendedBlock extendedBlock = new ExtendedBlock();
-        extendedBlock.id = unidentifiedBlock.getId();
-        extendedBlock.context = unidentifiedBlock.getContext();
-        if (unidentifiedBlock.getSourceLocation() != null) {
-            extendedBlock.sourceLine = unidentifiedBlock.getSourceLocation().getLineNumber();
+
+        extendedBlock.id = block.getId();
+        extendedBlock.context = block.getContext();
+        if (block.getSourceLocation() != null) {
+            extendedBlock.sourceLine = block.getSourceLocation().getLineNumber();
         }
         switch (extendedBlock.context) {
             case "image":
-                extendedBlock.target = (String) unidentifiedBlock.getAttributes().get("target");
+                extendedBlock.target = (String) block.getAttributes().get("target");
                 break;
             case "paragraph":
-                extendedBlock.sourceText = ((BlockImpl) unidentifiedBlock).getSource();
+                extendedBlock.sourceText = ((BlockImpl) block).getSource();
+                checkFootnotes(extendedBlock.sourceText);
                 break;
             case "table"://TODO: don't forget me
                 break;
 
         }
+//        if (!(extendedBlock.context.equals("section") ||
+//                extendedBlock.context.endsWith("list") ||
+//                extendedBlock.context.equals("table"))) {
+        allBlocks.add(extendedBlock);
+//        }
+        if (extendedBlock.id == null) {
+            unidentifiedBlocks.add(extendedBlock);
+        }
 
-        unidentifiedBlocks.add(extendedBlock);
-
-        checkNestedItemsId(unidentifiedBlock);
+        checkNestedItems(block);
 
 
     }
 
-    private static void checkNestedItemsId(StructuralNode block) {
+    private static void checkNestedItems(StructuralNode block) {
         if (block.getContext().endsWith("list")) {
 
             for (Object listItem : ((ListImpl) block).getItems()) {
-//                FIXME: фильтровать с Id == null
+
                 addListItem((ListItem) listItem);
+
             }
         } else if (block.getContext().equals("table")) {
             // TODO: table
-            System.out.println("TODO");
         }
     }
-
 
     private static void addListItem(ListItem listItem) {
         ExtendedBlock extendedBlock = new ExtendedBlock();
 
-        extendedBlock.id = listItem.getId(); // FIXME: Так не работает, надо разбирать строку
+        extendedBlock.id = getListItemId(listItem);
+
         extendedBlock.context = listItem.getContext();
         if (listItem.getSourceLocation() != null) {
             extendedBlock.sourceLine = listItem.getSourceLocation().getLineNumber();
@@ -117,7 +125,53 @@ public class Main {
         }
         extendedBlock.sourceText = listItem.getSource();
 
-        unidentifiedBlocks.add(extendedBlock);
+        checkFootnotes(extendedBlock.sourceText);
+
+//        if (!(extendedBlock.context.equals("section") ||
+//                extendedBlock.context.endsWith("list") ||
+//                extendedBlock.context.equals("table"))) {
+        allBlocks.add(extendedBlock);
+//        }
+        if (extendedBlock.id == null) {
+            unidentifiedBlocks.add(extendedBlock);
+        }
+
 
     }
+
+    private static String getListItemId(ListItem listItem) {
+        Pattern InlineAnchorRx = Pattern.compile("(?:\\\\)?(?:\\[\\[([\\p{Alpha}_:][\\w:.-]*)(?:, *(.+?))?]]|anchor:([\\p{Alpha}_:][\\w:.-]*)\\[(?:]|(.*?[^\\\\])])).*");
+        Matcher m = InlineAnchorRx.matcher(listItem.getSource());
+        if (m.matches()) {
+            if (m.group(1) != null) {
+                return m.group(1);
+            } else {
+                return m.group(3);
+            }
+        } else
+            return null;
+    }
+
+    private static void checkFootnotes(String sourceText) {
+
+        if (sourceText.contains("footnote")) {//TODO: убрать условие
+
+            Pattern InlineFootnoteMacroRx = Pattern.compile(".*\\\\?(footnote(?:ref)?):\\[(.*?[^\\\\])].*", Pattern.MULTILINE);
+            Matcher m = InlineFootnoteMacroRx.matcher(sourceText);
+
+            if (m.matches()) {
+                if (m.group(1).equals("footnote")) {
+                    System.out.println("footnote");
+                    String footnoteId = null;
+                    String footnoteText = m.group(2);
+                } else {
+                    System.out.println("footnoteref");
+                }
+
+            }
+        }
+    }
+
+
+//TODO: add footnote as block
 }
