b5a3d64aa21169ab7e46f1efd55f4ae20b5ab2cc
Anna Shainurova <amphiphile@yandex.ru>
2018-05-29 15:03:07 +0300

    обработка аргументов из командной строки

--- src/main/java/com/amphiphile/Main.java
+++ src/main/java/com/amphiphile/Main.java
@@ -1,46 +1,52 @@
 package com.amphiphile;
 
+import org.apache.commons.cli.*;
+
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException, IOException {
+        Options options = new Options();
+        options.addRequiredOption("i", "input", true, "input file to parse");
+        options.addRequiredOption("o", "output", true, "output file to write");
+        options.addRequiredOption("j", "json", true, "input file to parse");
 
-        String adocFilePath = System.getProperty("input");
-        if (adocFilePath == null) {
-            throw new IllegalArgumentException("input is a required property");
-        }
-        String jsonFilePath = System.getProperty("json");
-        if (jsonFilePath == null) {
-            throw new IllegalArgumentException("json is a required property");
-        }
+        options.addOption("pli", "parse-list-items", false, "Parse list items to separate elements");
+        options.addOption("pbi", "parse-biblio-items", false, "Parse bibliography list items to separate elements");
+        options.addOption("pc", "parse-cells", false, "Parse cells to separate element");
+        options.addOption("ili", "id-list-items", false, "Identify list items");
+        options.addOption("ibi", "id-biblio-items", false, "Identify bibliography list items");
+        options.addOption("ic", "id-cells", false, "Identify cells");
 
-        Boolean parseListItems = Boolean.parseBoolean(System.getProperty("parse-list-items", "true"));
-        Boolean parseBiblioItems = Boolean.parseBoolean(System.getProperty("parse-biblio-items", "true"));
-        Boolean parseCells = Boolean.parseBoolean(System.getProperty("parse-cells", "false"));
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmdLine = parser.parse(options, args);
+
+
+        String adocFilePath = cmdLine.getOptionValue("input");
+
+        Boolean parseListItems = Boolean.parseBoolean(cmdLine.getOptionValue("parse-list-items", "true"));
+        Boolean parseBiblioItems = Boolean.parseBoolean(cmdLine.getOptionValue("parse-biblio-items", "true"));
+        Boolean parseCells = Boolean.parseBoolean(cmdLine.getOptionValue("parse-cells", "false"));
 
         Lurker lurker = new Lurker(adocFilePath);
         ArrayList<ExtendedBlock> allBlocks = lurker.lurk(parseListItems, parseBiblioItems, parseCells);
 
         System.out.printf("All blocks: %d%n", allBlocks.size());
 
-        String outFilePath = System.getProperty("output");
-        if (outFilePath == null) {
-            throw new IllegalArgumentException("output is a required property");
-        }
-        Boolean identifyListItems = Boolean.parseBoolean(System.getProperty("id-list-items", "true"));
-        Boolean identifyBiblioItems = Boolean.parseBoolean(System.getProperty("id-biblio-items", "true"));
-        Boolean identifyCells = Boolean.parseBoolean(System.getProperty("id-cells", "false"));
-        try {
-            Extender extender = new Extender(adocFilePath, outFilePath, jsonFilePath, allBlocks);
-            extender.extend(identifyListItems, identifyBiblioItems, identifyCells);
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
-        }
+        String outFilePath = cmdLine.getOptionValue("output");
+        String jsonFilePath = cmdLine.getOptionValue("json");
+
+        Boolean identifyListItems = Boolean.parseBoolean(cmdLine.getOptionValue("id-list-items", "true"));
+        Boolean identifyBiblioItems = Boolean.parseBoolean(cmdLine.getOptionValue("id-biblio-items", "true"));
+        Boolean identifyCells = Boolean.parseBoolean(cmdLine.getOptionValue("id-cells", "false"));
+
+        Extender extender = new Extender(adocFilePath, outFilePath, jsonFilePath, allBlocks);
+        extender.extend(identifyListItems, identifyBiblioItems, identifyCells);
 
         System.out.println("ycnex!");
 
     }
-
 }
