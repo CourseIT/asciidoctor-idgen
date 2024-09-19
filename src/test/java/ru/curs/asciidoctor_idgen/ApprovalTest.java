8658747f35cdc17bc1d308f8dc9a50ca78838184
fiddlededee <consulting@yandex.ru>
2024-09-20 00:15:09 +0300

    fix: Убрана ошибка двойного проставления id

--- src/test/java/ru/curs/asciidoctor_idgen/ApprovalTest.java
+++ src/test/java/ru/curs/asciidoctor_idgen/ApprovalTest.java
@@ -6,9 +6,7 @@ import org.approvaltests.namer.NamerFactory;
 import org.jsoup.Jsoup;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,31 +15,37 @@ class ApprovalTest {
 
     @ParameterizedTest
     @CsvSource({
-            "list-1",
-            "error-1",
-            "list-2",
+            "existing-id-1, adoc",
+            "list-1, adoc",
+            "list-1, html",
+            "list-1, log",
+            "error-1, log",
+            "list-2, adoc",
+            "section-1, adoc",
     })
-    void enrich(String adocCase) throws IOException {
+    void enrich(String adocCase, String type) throws Exception {
         Path tmpdir = Files.createTempDirectory(Paths.get("target"), "approve");
         String adocFilePath = "src/test/data/%s.adoc".formatted(adocCase);
-        Lurker lurker = new Lurker(adocFilePath);
         String outFilePath = "%s/%s-out.adoc".formatted(tmpdir, adocCase);
         String htmlFilePath = "%s/%s-out.html".formatted(tmpdir, adocCase);
         String jsonFilePath = "%s/%s-json.adoc".formatted(tmpdir, adocCase);
         var log = Main.enrich(adocFilePath, outFilePath, jsonFilePath, true);
         File adocFile = new File(outFilePath);
-        NamerFactory.setAdditionalInformation(adocCase);
-        Approvals.verify(
-                FileUtils.readFile(adocFile)
-                        .replaceAll("[a-z0-9]{6}]]", "FFFFFF]]")
-        );
-        NamerFactory.setAdditionalInformation(adocCase + ".html");
-        Approvals.verify(
-                Jsoup.parse(new File(htmlFilePath)).selectXpath("//div[@id='content']").get(0).toString()
-                        .replaceAll("_[a-z0-9]{6}\"", "_FFFFFF\"")
-        );
-        NamerFactory.setAdditionalInformation(adocCase + ".log");
-        Approvals.verify(log.toString().replaceAll("approve[0-9]+", "approve"));
-
+        if ("adoc".equals(type)) {
+            NamerFactory.setAdditionalInformation(adocCase);
+            Approvals.verify(
+                    FileUtils.readFile(adocFile)
+                            .replaceAll("[a-z0-9]{6}]]", "FFFFFF]]")
+            );
+        } else if ("html".equals(type)) {
+            NamerFactory.setAdditionalInformation(adocCase + ".html");
+            Approvals.verify(
+                    Jsoup.parse(new File(htmlFilePath)).selectXpath("//div[@id='content']").get(0).toString()
+                            .replaceAll("_[a-z0-9]{6}\"", "_FFFFFF\"")
+            );
+        } else if ("log".equals(type)) {
+            NamerFactory.setAdditionalInformation(adocCase + ".log");
+            Approvals.verify(log.toString().replaceAll("approve[0-9]+", "approve"));
+        } else throw new Exception("Can't recoginze test type");
     }
 }
